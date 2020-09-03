7d0ee727906f213ab1d1b7e96f4ae9db0022ae98
ggauti <67849427+ggauti@users.noreply.github.com>
2020-09-03 12:00:27 +0000

    Add files via upload

---
+++ sequence.py
@@ -0,0 +1,7 @@
+# ask user for the lengt of the sequens that they wan to generat
+# generate sequens that takes the last 3 numbers of the sequence and adthem together for the next number in the sequence
+# print the seaquens
+sequence = 0
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+while n > len(sequence):
+    sequence = 
